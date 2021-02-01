adcf3bc3911e9a7453c822c5d7f382cae05150f9
Benjamin Pelletier <BenjaminPelletier@users.noreply.github.com>
2021-02-01 12:18:20 -0800

    Update reverse_proxy.sh
    Fix location of nginx.conf

--- reverse_proxy.sh
+++ reverse_proxy.sh
@@ -5,7 +5,7 @@ docker run \
   --log-opt max-file=10 \
   --name reverseproxy \
   --restart always \
-  -v `pwd`/nginx/nginx.conf:/etc/nginx/nginx.conf \
+  -v `pwd`/nginx.conf:/etc/nginx/nginx.conf \
   -v `pwd`/nginx/conf:/etc/nginx/conf.d \
   -v `pwd`/nginx/letsencrypt:/etc/letsencrypt \
   -p 80:80 -p 443:443 -d benpelletier/reverse_proxy
